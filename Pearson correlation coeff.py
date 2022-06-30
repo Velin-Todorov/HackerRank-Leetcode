187d85de999a917e2f6ae05fce29307638339cd9
Velin-Todorov <velintodorov@yahoo.com>
2022-06-30 20:24:23 +0300

    HackerRank problem

--- Pearson correlation coeff.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-n = int(input())
-A = np.array([float(x) for x in input().split()], int)
-B = np.array([float(x) for x in input().split()], int)
-
-print(np.corrcoef(A, B)[0][1])
-
