a77537eed83103ef584c4a2574536edf32b2c0a6
Velin-Todorov <velintodorov@yahoo.com>
2022-07-25 19:33:27 +0200

    HackerRank Problem

---
+++ Matching Whitespace & Non-Whitespace Character.py
@@ -0,0 +1,9 @@
+"""
+Link: https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character/problem?isFullScreen=true
+"""
+
+Regex_Pattern = r"\S{2}\s\S{2}\s\S{2}"	 # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
