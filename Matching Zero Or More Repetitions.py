d3fd0b23d267862f719cb450c864a8cff62b8fbe
Velin-Todorov <velintodorov@yahoo.com>
2022-07-25 19:40:07 +0200

    HackerRank Problem

---
+++ Matching Zero Or More Repetitions.py
@@ -0,0 +1,8 @@
+"""
+Link https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem?isFullScreen=true
+"""
+Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'	 # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
