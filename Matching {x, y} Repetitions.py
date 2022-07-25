b77454d9398f20d0bc5bfe6ca4436f7cab52e333
Velin-Todorov <velintodorov@yahoo.com>
2022-07-25 19:38:34 +0200

    HackerRank Problem

---
+++ Matching {x, y} Repetitions.py
@@ -0,0 +1,9 @@
+"""
+Link: https://www.hackerrank.com/challenges/matching-x-y-repetitions/problem?isFullScreen=true
+"""
+
+Regex_Pattern = r'^\d{1,2}[a-zA-Z]{3,}[.]{,3}$'	 # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
