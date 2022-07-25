1d47d1555d83e95cf9e5514587a203715b557289
Velin-Todorov <velintodorov@yahoo.com>
2022-07-25 19:28:53 +0200

    HackerRank Problem

---
+++ Matching Anything But a Newline.py
@@ -0,0 +1,14 @@
+""""
+Link: https://www.hackerrank.com/challenges/matching-anything-but-new-line/problem?isFullScreen=true
+"""
+
+regex_pattern = r"^...\....\....\....$"	  # Do not delete 'r'.
+
+import re
+import sys
+
+test_string = input()
+
+match = re.match(regex_pattern, test_string) is not None
+
+print(str(match).lower())
