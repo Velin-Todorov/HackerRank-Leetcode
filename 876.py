619a1100f2b0c5cd617266533c8207883df8994e
Velin-Todorov <velintodorov@yahoo.com>
2023-03-04 18:33:54 +0100

    Solved 876

--- 876.py
+++ 876.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+
+def middleNode(head):
+
+    temp = head
+    size = 0
+
+    while temp is not None:
+        size += 1
+        temp = temp.next
+
+    mid_el = size // 2
+    
+    count = 0
+
+    node = None
+    temp = head
+
+    while temp is not None:
+        if count == mid_el:
+            node = temp
+            break
+        count += 1
+        temp = temp.next
+    
+    if node:
+        return node
+
 
