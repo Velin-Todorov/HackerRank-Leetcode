07784d4c900ef6525f591ddf7994f5adf5428b22
Velin-Todorov <velintodorov@yahoo.com>
2023-03-05 15:28:46 +0100

    Added comment for myself

--- Insertion_Sort_II.py
+++ Insertion_Sort_II.py
@@ -6,9 +6,9 @@ def insertionSort(n, arr):
 
         j = i - 1
 
-        while j >= 0 and arr[j] > current:
-            arr[j + 1] = arr[j]
-            j -= 1
+        while j >= 0 and arr[j] > current: #here we check if the element left to the current el is bigger. If yes:
+            arr[j + 1] = arr[j] #We shift the left element to the right and there is a gap in the array
+            j -= 1 #We move to the next left element, until we reach 0 or until we current is bigger that than the left element
 
-        arr[j + 1] = current
+        arr[j + 1] = current # fill the gap with the current_element
 
